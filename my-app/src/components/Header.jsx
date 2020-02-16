42c808cbb634742708fd5be6d70ca6aa8f0e7c5a
patrickspafford <patrickspafford1@gmail.com>
2020-02-16 02:04:44 -0600

    Last commit directly to master --> Created function in Header.jsx with boilerplate code.

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+
+export default function Header() {
+
+
+    return (
+        <div>
+          <AppBar>
+            Patrick Spafford
+          </AppBar>  
+        </div>
+    )
+}
