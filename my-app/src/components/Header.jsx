bc06e0f95cf528e0f34be022d1214fedf36e370f
patrickspafford <patrickspafford1@gmail.com>
2020-02-16 19:23:34 -0600

    Adds and styles the AppBar in Header

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/IconButton';
 import AppBar from '@material-ui/core/AppBar';
+import DarkModeIcon from '@material-ui/icons/BrightnessMedium';
+import './Header.css'
+import { withStyles } from '@material-ui/core';
 
-
+const StyledAppBar = withStyles({
+  colorPrimary: 
+  {
+    backgroundColor: '#D45E25'
+  }
+})(AppBar);
 export default function Header() {
 
-
     return (
-        <div>
-          <AppBar>
-            Patrick Spafford
-          </AppBar>  
-        </div>
+      <header>
+          <StyledAppBar classes position='fixed'>
+            <div className='appBar'>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+              <h3>Patrick Spafford</h3>
+              <IconButton>
+                <DarkModeIcon />
+              </IconButton>
+            </div>
+          </StyledAppBar>  
+      </ header>
     )
 }
