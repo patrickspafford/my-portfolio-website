b49b0a7a38e8cbda6756d6d088aa9290094524d9
patrickspafford <patrickspafford1@gmail.com>
2020-02-26 09:12:24 -0600

    Creates empty Book component and its corr. CSS file

--- src/pages/BookshelfPage.jsx
+++ src/pages/BookshelfPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography } from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, 
+ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function BookshelfPage() {
