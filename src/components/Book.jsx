ae7406906b64591ed98205121e8877d57ff8a4b7
patrickspafford <patrickspafford1@gmail.com>
2020-03-09 21:19:19 -0500

    Implements scrolling library of books in Bookshelf

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,33 +1,64 @@
 import React from 'react';
-import { Card, CardHeader, CardMedia, CardContent, withStyles } from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardContent, withStyles, ExpansionPanel, ExpansionPanelSummary,
+ExpansionPanelDetails } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const StyledCardMedia = withStyles({
     root : {
         padding: '10px',
         width: '150px',
-        margin: 'auto'
+        margin: 'auto',
     },
     img: {
         objectFit: 'scale-down',
     }
     })(CardMedia);
+const StyledCardContent = withStyles({
+    root: {
+        backgroundColor: '#eee'
+    }
+})(CardContent);
 
 const StyledCardHeader = withStyles({
     content: {
         textAlign: 'center'
+    },
+    root: {
+        backgroundColor: '#142630',
+        color: 'white'
     }
 })(CardHeader);
 
+const StyledExpansionPanelSummary = withStyles({
+root: {
+    backgroundColor: '#142630',
+    color: 'white'
+}
+})(ExpansionPanelSummary);
+
+const StyledCard = withStyles({
+    root: {
+        width: '350px'
+    }
+})(Card);
+
 export default function Book({title, image, desc}) {
     return (
-        <Card>
+        <div style={{margin: '25px'}}>
+        <StyledCard>
             <StyledCardHeader title={title} />
-            <CardContent>
-                <StyledCardMedia image={image} height='100px' width='100px' component='img'/>
-            </CardContent>
-            <CardContent>
+            <StyledCardContent>
+                <StyledCardMedia image={image} height='150px' component='img'/>
+            </StyledCardContent>
+        </StyledCard>
+        <ExpansionPanel>
+            <StyledExpansionPanelSummary expandIcon={<ExpandMoreIcon style={{color: 'white'}} />}>
+                <p>About</p>
+            </StyledExpansionPanelSummary>
+            <ExpansionPanelDetails>
                 <p>{desc}</p>
-            </CardContent>
-        </Card>
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+        </div>
     )
 }
--- src/pages/BookshelfPage.jsx
+++ src/pages/BookshelfPage.jsx
@@ -1,27 +1,73 @@
 import React from 'react';
 import Header from '../components/Header';
 import Book from '../components/Book';
-import {ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography } from '@material-ui/core';
+import {ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, createMuiTheme, ThemeProvider} from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SHHML from '../pictures/SheHasHerMothersLaugh.jpg';
 
+const theme = createMuiTheme({
+    overrides: {
+      MuiCollapse: {
+        container: {
+          overflow: 'scroll',
+        },
+        entered: {
+            overflow: 'auto'
+        }
+      },
+    },
+  });
 export default function BookshelfPage() {
     const expansionPanels = [
         {
             title: "Notable Audiobooks",
-            content: "Placeholder...",
             books: [
                 {
                     title: "She Has Her Mother's Laugh",
                     description: "A book on genetics",
                     img: SHHML
                 },
+                {
+                    title: "Algorithms to Live By",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "The Happiness Hypothesis",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "Infinite Powers",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "She Has Her Mother's Laugh",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "She Has Her Mother's Laugh",
+                    description: "A book on genetics",
+                    img: SHHML
+                },
+                {
+                    title: "She Has Her Mother's Laugh",
+                    description: "A book on genetics",
+                    img: SHHML
+                }
+                
                 
             ]
         },
         {
             title: "Notable Printed Books",
-            content: "Other books...",
             books: []
         }
     ]
@@ -30,6 +76,7 @@ export default function BookshelfPage() {
             <Header AppBarStyle="static" />
             {expansionPanels.map((expansionPanel, i) => {
                 return (
+            <ThemeProvider theme={theme}>
                 <ExpansionPanel key={i}>
                     <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography>
@@ -38,9 +85,11 @@ export default function BookshelfPage() {
                     </ExpansionPanelSummary>
                     <ExpansionPanelDetails>
                         {expansionPanel.books.map(book =>
-                        <Book key={book.title} title={book.title} image={book.img} desc={book.description} />)}   
+                        <Book key={book.title} title={book.title} image={book.img} desc={book.description} />)}
                     </ExpansionPanelDetails>
-                </ExpansionPanel>)
+                </ExpansionPanel>
+                </ThemeProvider>
+                )
             })}  
         </div>
     )
